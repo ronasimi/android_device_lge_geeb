373a5102de0291a03db0eaf7263af6881a08690f
Paulicat <antonpaulic@gmail.com>
2014-02-28 09:14:38 -0500

    remove prebuilt kernel stuffs

--- device.mk
+++ device.mk
@@ -35,15 +35,6 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     charger
 
-ifneq ($(findstring svelte, $(TARGET_PRODUCT)),)
-LOCAL_KERNEL := device/lge/geeb_svelte-kernel/kernel
-else
-LOCAL_KERNEL := device/lge/geeb-kernel/kernel
-endif
-
-PRODUCT_COPY_FILES := \
-	$(LOCAL_KERNEL):kernel
-
 PRODUCT_COPY_FILES += \
 	device/lge/geeb/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
 	device/lge/geeb/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
