776a6c3637f6bedb2468700368560bca6ba02f40
Paulicat <antonpaulic@gmail.com>
2014-02-28 22:13:49 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,8 @@ TARGET_KERNEL_CONFIG := geeb_defconfig
 
 TARGET_OTA_ASSERT_DEVICE := mako,geeb,gee_a,e970,gee,geebus,e973
 
-TARGET_MPDECISION_BOOST_SOCKET := /dev/socket/mpdecision/touchboost
+# Turn off touchboost logcat spamfest, touchboost still happens.
+TARGET_POWERHAL_NO_TOUCH_BOOST := true
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
